66d4b3322b83833a093152eb6a0d58d85f2fb6e4
Goffy <dev@goffy.cc>
2024-02-02 15:48:35 +0800

    Feat: changes the okhttp readTimeout default time to 60000

--- src/main/java/io/gopluslabs/client/ApiClient.java
+++ src/main/java/io/gopluslabs/client/ApiClient.java
@@ -79,6 +79,7 @@ public class ApiClient {
      */
     public ApiClient() {
         httpClient = new OkHttpClient();
+        httpClient.setReadTimeout(60000,TimeUnit.MILLISECONDS);
 
 
         verifyingSsl = true;
